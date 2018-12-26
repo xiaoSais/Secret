0eddbd394dab6bcc33423731e9fc109c324df9cb
sai.shi <sai.shi@vipshop.com>
2018-12-26 13:56:22 +0800

    m

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  title: 'Secret',
+  description: 'Just playing around',
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    displayAllHeaders: true,
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: '技术文档', link: '/blog/document/swiper' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      {
+        title: '技术文档', // 侧边栏名称
+        children: [
+          '/blog/document/swiper', // 你的md文件地址
+          '/blog/document/infiniate',
+          '/blog/document/YUIDoc',
+          '/blog/document/mini',
+          '/blog/document/fenxi'
+        ]
+      },
+      {
+        title: '散文随笔', // 侧边栏名称
+        children: [
+          '/blog/article/notitle', // 你的md文件地址
+          '/blog/article/cd',
+          '/blog/article/sh',
+          '/blog/article/nj'
+        ]
+      },
+     ]
+  }
+}
